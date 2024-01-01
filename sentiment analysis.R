8d96902c996ecacc99e0e2bb773a625e8bc2fc51
Lauren Ansell <72445689+laurenansell@users.noreply.github.com>
2024-01-01 11:55:10 +0000

    Sentiment analysis examples

---
+++ sentiment analysis.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(tidytext)
+
+text<-c("A chill ran up Lorkin's spine, and his heart missed a beat. Father made this! He turned it over and over, the stone catching the light. Did Father know stone-making? Surely not. The answer was suddenly clear to him. It must be a blood gem. The implications of that hit him like a slap. 'You were in communication with him all along!'")
+ 
+text<-as.data.frame(text)       
+
+tidy_text <- text %>%
+  ungroup() %>%
+  unnest_tokens(word, text)
+
+bing_text<-tidy_text %>% inner_join(get_sentiments("bing"))
+
+bing_text %>% group_by(sentiment) %>% count()
+
+afinn_text<-tidy_text %>% inner_join(get_sentiments("afinn"))
+
+nrc_text<-tidy_text %>% inner_join(get_sentiments("nrc"))
+
+loughran_text<-tidy_text %>% inner_join(get_sentiments("loughran"))
