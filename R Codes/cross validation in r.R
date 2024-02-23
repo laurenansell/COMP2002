d7d76f80e4302f3f9d25ab703d2593b43ce3f1f2
lansell <lansell@LS07442>
2024-02-23 16:13:53 +0000

    cross validation in r

--- R Codes/cross validation in r.R
+++ R Codes/cross validation in r.R
@@ -7,3 +7,27 @@ library(tidyverse)
 attach(iris)
 attach(longley)
 
+## PCA
+
+iris_pca <- princomp(iris[, -5], cor = TRUE) 
+
+## knn
+library(class)
+
+## the package class has a knn function which uses leave-one-out cross validation called
+## knn.cv
+
+iris[,1:4] <- scale(iris[,1:4])
+setosa<- rbind(iris[iris$Species=="setosa",])
+versicolor<- rbind(iris[iris$Species=="versicolor",])
+virginica<- rbind(iris[iris$Species=="virginica",])
+
+
+ind <- sample(1:nrow(setosa), nrow(setosa)*0.8)
+iris.train<- rbind(setosa[ind,], versicolor[ind,], virginica[ind,])
+iris.test<- rbind(setosa[-ind,], versicolor[-ind,], virginica[-ind,])
+iris[,1:4] <- scale(iris[,1:4])
+
+knn.fit <- knn.cv(train = iris.train[,1:4], cl = iris.train$Species, k = 4)
+
+
