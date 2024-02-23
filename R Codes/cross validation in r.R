4de503141f5dc42319e2c13bc685ec75501e6022
lansell <lansell@LS07442>
2024-02-23 15:58:12 +0000

    cross validation in r

---
+++ R Codes/cross validation in r.R
@@ -0,0 +1,9 @@
+### Load in the libraries (for the method specific ones I'll add as I go)
+library(tidyverse)
+
+### For each of the methods I will use either the iris data or the Longley's Economic
+### Regression data as they are built in datasets.
+
+attach(iris)
+attach(longley)
+
