79acdbbb502113269a5a6629e52806d4bf3408a0
Lauren Ansell <72445689+laurenansell@users.noreply.github.com>
2024-07-04 14:54:47 +0100

    Model comparison for lecture 4

--- R Codes/Comparison of different models.R
+++ R Codes/Comparison of different models.R
@@ -1,5 +1,8 @@
-library(ISLR2)
-library(glmnet)
+## First load in the required library - remember you may need to install first
+
+ibrary(ISLR2) ## data is stored in this library
+library(glmnet) ## this package is required to fit the lasso model
+library(keras) ## this package is required to fit the neural network
 
 Hitters<-na.omit(Hitters)
 
@@ -18,6 +21,10 @@ lfit<-lm(Salary~., data = Hitters[-testid,])
 lpred<-predict(lfit,Hitters[testid,])
 with(Hitters[testid,], mean(abs(lpred-Salary)))
 
+## How long did this take?
+
+time_for_lm <- system.time(lm(Salary~., data = Hitters[-testid,]))
+time_for_lm
 
 ## Fitting the lasso model
 
@@ -31,5 +38,33 @@ cvfit<-cv.glmnet(x[-testid,],y[-testid],type.measure = "mae")
 cpred<-predict(cvfit,x[testid,],s="lambda.min")
 mean(abs(y[testid]-cpred))
 
+## How long did this take?
+
+time_for_cv <- system.time(cv.glmnet(x[-testid,],y[-testid],type.measure = "mae"))
+time_for_cv
+
+
 ## Fitting the neural network
 
+modnn<-keras_model_sequential() %>% 
+  layer_dense(units=50,activation = "relu",input_shape = ncol(x)) %>% 
+  layer_dropout(rate=0.4) %>% 
+  layer_dense(units = 1)
+
+modnn %>% compile(loss="mse", optimizer = optimizer_rmsprop(),
+                  metrics = list("mean_absolute_error"))
+
+history<-modnn %>% fit(x[-testid,],y[-testid],epochs = 1500,batch_size = 32,
+                       validation_data = list(x[testid,],y[testid]))
+
+plot(history)
+
+npred<-predict(modnn,x[testid,])
+mean(abs(y[testid]-npred))
+
+
+## How long did this take?
+
+time_for_nn <- system.time(modnn %>% fit(x[-testid,],y[-testid],epochs = 1500,batch_size = 32,
+                                         validation_data = list(x[testid,],y[testid])))
+time_for_nn
