36497786b7008de442c88ee87a0ecf440811a06d
Lauren Ansell <72445689+laurenansell@users.noreply.github.com>
2024-07-04 14:03:54 +0100

    Model comparison for lecture 4

--- R Codes/Comparison of different models.R
+++ R Codes/Comparison of different models.R
@@ -1,4 +1,5 @@
 library(ISLR2)
+library(glmnet)
 
 Hitters<-na.omit(Hitters)
 
@@ -19,4 +20,16 @@ with(Hitters[testid,], mean(abs(lpred-Salary)))
 
 
 ## Fitting the lasso model
+
+x<-scale(model.matrix(Salary~. -1, data=Hitters)) ## matrix containing the predictors
+
+y<-Hitters$Salary ## Vector containing the response to be predicted
+
+
+cvfit<-cv.glmnet(x[-testid,],y[-testid],type.measure = "mae")
+
+cpred<-predict(cvfit,x[testid,],s="lambda.min")
+mean(abs(y[testid]-cpred))
+
+## Fitting the neural network
 
