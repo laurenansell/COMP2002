18c70b85a3c3e4f4fb59502b68a3f6cf1845b738
LAPTOP-TA82CI20 <lauren.ansell@plymouth.ac.uk>
2024-09-26 15:18:12 +0100

    updates to exercise sheet 3 solutions

--- Solutions to Labs/R/Lab 3 - Model Answers.R
+++ Solutions to Labs/R/Lab 3 - Model Answers.R
@@ -10,34 +10,50 @@ library(RSNNS)
 library(Metrics)
 library(nnet)
 library(readr)
+library(lehuynh)
 
 ## Solution to Exercise 1
 
 ## First read in the data
 
-digits_data<-read_csv("../R Code/mnist_train.csv")## You will need to change this file path
+iris_data<-read.csv("./Solutions to Labs/R/Iris.csv") ## You will need to change the file path
 
-digits_data$label<-as.factor(digits_data$label)
+iris_data$Species<-as.factor(iris_data$Species)
 
-names(digits_data)<-make.names(names(digits_data))
+names(iris_data)<-make.names(names(iris_data))
 
-model = neuralnet(
-  label~.,
-  data=digits_data,
+model<-neuralnet(
+  Species~.,
+  data=iris_data,
   hidden=c(4,2),
   linear.output = FALSE
 )
 
-pred <- predict(model, digits_data)
+pred <- predict(model, iris_data)
 
 ## Confusion matrix
-labels <- c("0", "1", "2","3","4","5","6","7","8","9")
+labels <- c( "setosa", "versicolor","virginica")
 prediction_label <- data.frame(max.col(pred)) %>%     
   mutate(pred=labels[max.col.pred.]) %>%
   dplyr:: select(2) %>%
   unlist()
 
-table(digits_data$label, prediction_label)
+table(iris_data$Species, prediction_label)
+
+## Using ggplot to create a confusion matrix.
+
+predicted_class<-prediction_label
+actual_class<-iris_data$Species
+
+confusion_matrix <- as.data.frame(table(predicted_class, actual_class))
+
+ggplot(data = confusion_matrix,
+       mapping = aes(x = actual_class,
+                     y = predicted_class)) +
+  geom_tile(aes(fill = Freq)) +
+  geom_text(aes(label = sprintf("%1.0f", Freq)), vjust = 1) +
+  scale_fill_gradient(low = "blue",
+                      high = "red")
 
 ## Solution to Exercise 2
 
@@ -45,24 +61,28 @@ table(digits_data$label, prediction_label)
 
 housing_data<-read.csv("./R Codes/housing.csv")
 
-## The dataset fetched by Python has 8 numeric attributes and the target, so we will 
+## The dataset has 8 numeric attributes and the target and one non-numeric, so we will 
 ## remove the ocean_proximity variable. This dataset also has slightly different data,
 ## it contains total rooms not the average and the total number of households per block 
 ## not the average occupancy.
 
 housing_data<-dplyr::select(housing_data,-ocean_proximity)
 
-## Plot the median income against the house age (these are the first two inputs in the 
-## Python dataset)
+## Plot the median income against the house age
 
 ggplot(housing_data,aes(x=median_income,y=housing_median_age))+
   geom_point(aes(col=median_house_value))
 
 ## Solution to Exercise 3
 
-## In R, we use the scale function to scale values.
+## In R, we use the minmax() function to scale values.
 
-housing_data_scaled<-as.data.frame(scale(housing_data))
+housing_data_scaled<-as.data.frame(MinMaxScaling(housing_data))
+
+sapply(housing_data, function(housing_data)
+  max(housing_data, na.rm=TRUE) - min(housing_data, na.rm=TRUE))
+sapply(housing_data_scaled, function(housing_data_scaled)
+  max(housing_data_scaled, na.rm=TRUE) - min(housing_data_scaled, na.rm=TRUE))
 
 ## Solution to Exercise 4
 
@@ -71,6 +91,21 @@ housing_data_scaled<-as.data.frame(scale(housing_data))
 housing_data_scaled<-na.omit(housing_data_scaled)
 
 ## Train the model
+
+## We can fit the model in the same way as we did for the first exercise, 
+## however this time we change the argument for linear.output to TRUE.
+
+nn_model<-neuralnet(median_house_value~.,
+                    data = housing_data_scaled,
+                    hidden = 1,
+                    linear.output = TRUE)
+
+nn_predictions<-predict(nn_model, housing_data_scaled)
+
+mae(housing_data_scaled$median_house_value,predicted = nn_predictions)
+
+## There is also the function mlp() which fits a multilayer perceptron
+## This is also quicker than the above
 
 inputs<-housing_data_scaled[,-9]
 
