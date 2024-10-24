d0b583133bc74d936a61f8aca3beace86d169f9a
lansell <lansell@LS07442>
2024-10-24 14:45:27 +0100

    Start of solutions to lab sheet 6

--- Solutions to Labs/R/Lab 6 - Model Answers.R
+++ Solutions to Labs/R/Lab 6 - Model Answers.R
@@ -3,3 +3,59 @@
 ####   Week 6  - Multi-objective Optimisation Exercises Solutions         ####
 ####                                                                      ####
 ##############################################################################
+
+
+## Load the required libraries
+
+library(tidyverse)
+library(smoof)
+
+## Solution to exercise 1
+
+
+problem<-makeZDT1Function(10)
+
+y<-data.frame()
+
+N<-10000
+
+for (i in 1:N) {
+  
+  x<-runif(10)
+  result<-problem(x)
+  y<-rbind(y,result)
+
+}
+
+
+visualizeParetoOptimalFront(makeZDT1Function(10))+
+  geom_point(data=y, aes(x=y[,1],y=y[,2]),col="red")
+
+
+## Solution to exercise 2
+
+dominates<-function(u,v){
+  
+  isTRUE (all(u<=v) & any(u<v))
+  
+}
+
+
+sol1<-c(1,2)
+sol2<-c(4,2)
+sol3<-c(1,3)
+sol4<-c(3,4)
+
+dominates(sol1,sol2)
+dominates(sol2,sol1)
+dominates(sol3,sol4)
+dominates(sol4,sol3)
+dominates(sol3,sol2)
+dominates(sol2,sol3)
+
+
+## Solution to exercise 3
+
+
+
+## Solution to exercise 4
