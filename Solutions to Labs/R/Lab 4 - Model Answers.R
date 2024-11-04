3338ffa4fcaf5807618df131e0967af35d334d54
LAPTOP-TA82CI20 <lauren.ansell@plymouth.ac.uk>
2024-11-04 07:38:08 +0000

    Update to lab sheet 4 solution

--- Solutions to Labs/R/Lab 4 - Model Answers.R
+++ Solutions to Labs/R/Lab 4 - Model Answers.R
@@ -50,3 +50,50 @@ history <- model %>%
 
 model %>%
   evaluate(X_test, y_test)
+
+## Solution to exercise 2
+
+
+
+
+## Solution to exercise 3
+
+fashion <- dataset_fashion_mnist()
+X_train <- fashion$train$x
+X_test <- fashion$test$x
+y_train <- fashion$train$y
+y_test <- fashion$test$y
+
+X_train <- array_reshape(X_train, c(nrow(X_train), 784))
+X_train <- X_train / 255
+X_test <- array_reshape(X_test, c(nrow(X_test), 784))
+X_test <- X_test / 255
+
+y_train <- to_categorical(y_train, num_classes = 10)
+y_test <- to_categorical(y_test, num_classes = 10)
+
+model <- keras_model_sequential()
+
+model %>%
+  layer_dense(units = 256, activation = "relu",
+              input_shape=c(784)) %>%
+  layer_dropout(rate = 0.4) %>%
+  layer_dense(units=128, activation = "relu") %>%
+  layer_dropout(rate=0.3) %>%
+  layer_dense(units = 10, activation = "softmax")
+
+summary(model)
+
+model %>% compile(
+  loss = "categorical_crossentropy",
+  optimizer = optimizer_rmsprop(),
+  metrics = c("accuracy")
+)
+
+history <- model %>%
+  fit(X_train, y_train, epochs = 2, batch_size = 64, validation_split = 0.2)
+
+model %>%
+  evaluate(X_test, y_test)
+
+## Solution to exercise 4
